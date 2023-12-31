2c89286e973279d9cc7283a452492b39466cd046
Aditya Rajput <adityaudm@gmail.com>
2023-12-31 20:05:33 +0530

    nextFlix!! fixes 7

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -22,6 +22,7 @@
             await signIn('credentials',{
                 email,
                 password, 
+                redirect:true,
                 callbackUrl:'/profiles',
             });
  
